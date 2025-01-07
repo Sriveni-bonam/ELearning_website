43adbd7b7b0abd9d31e2371d59339503e888e64d
Sriveni-bonam <151802628+Sriveni-bonam@users.noreply.github.com>
2025-01-07 15:33:39 +0530

    jsquiz.js

--- ELearn/jsquiz.js
+++ ELearn/jsquiz.js
@@ -167,7 +167,7 @@ function showResults() {
 
 // Navigate to Courses Page
 function goToCoursesPage() {
-    window.location.href = "courses.html"; // Replace with the actual URL or page name
+    window.location.href = "c.html"; // Replace with the actual URL or page name
 }
 
 // Handle Submit Button Click
